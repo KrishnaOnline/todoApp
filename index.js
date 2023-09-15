9e91cd86ed13cc459bab95094299b0b9712b8a6b
KrishnaOnline <krishnavamshikusuma@gmail.com>
2023-09-15 16:27:16 +0530

    04

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ const dbConnect = require('./config/database');
 dbConnect();
 
 app.get('/', (req, res) => {
-    res.send(`<h1>This is The HOMEPAGE babey</h1>`)
+    res.send(`<h1>This is The HOMEPAGE</h1>`)
 })
